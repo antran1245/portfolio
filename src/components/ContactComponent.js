fb89f5366fe405c131f0b886df673df97faeb3a7
antran1245 <antran1245@gmail.com>
2020-10-17 19:50:33 -0700

    about me set up

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
+import {Card, CardBody, CardHeader} from 'reactstrap';
 import NavbarHeader from './NavbarComponent';
 
 class Contact extends Component {
     render() {
         return (
             <div className="background_image contact">
-                <div>
-
+                <div className="d-block d-sm-none">
+                    <NavbarHeader />
                 </div>
-                <NavbarHeader />
+                <div className="container contact_layout">
+                    <div className="row">
+                        <div className="col-12">
+                        
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="d-none d-sm-block">
+                    <NavbarHeader />
+                </div>
             </div>
         );
     }
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -6,15 +6,42 @@ class Home extends Component {
     render() {
         return (
             <div className="background_image home">
+                <div className="d-block d-sm-none">
+                    <NavbarHeader />
+                </div>
                 <div className="container home_layout">
                     <div className="row">
-                        <div className="col-12">
-                            <h1>Home</h1>
+                        <div className="col-12 col-sm-12 ml-sm-4">
+                            <h1 style={{fontSize:"80px", fontWeight:"bold"}}>Hello</h1>
                         </div>
                     </div>
-                    
+                    <div className="row">
+                        <div className="col-12 col-sm-12 ml-sm-4">
+                            <h2 style={{fontSize:"60px"}}>My Name Is An Tran</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-sm-12 ml-sm-4">
+                            <h2 style={{fontSize:"60px"}}>I'm a Coder</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-sm-12 ml-sm-4">
+                            <h2 style={{fontSize:"60px"}}>I'm a Web Designer</h2>
+                        </div>
+                    </div>
+                    <div className="row mt-sm-5 mt-3">
+                        <div className="col-12 col-sm-4">
+                            <p>Located in California.</p>
+                        </div>
+                        <div className="col-12 col-sm-4 offset-sm-4">
+                        <a href="#"><p><i className="fa fa-envelope-o fa-lg"></i> example1245@gmail.com</p></a>
+                        </div>
+                    </div>
                 </div>
-                <NavbarHeader />
+                <div className="d-none d-sm-block">
+                    <NavbarHeader />
+                </div>
             </div>
         );
     }
