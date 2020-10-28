882350967875a5395de9945c4cd4680b3a26900a
antran1245 <antran1245@gmail.com>
2020-10-28 12:19:13 -0700

    Updates

---
+++ precache-manifest.582812e104d9b95be927e391e0e2818f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28cc7c5ab547fd4c3c257121b9998e6",
+    "url": "/portfolio.git/index.html"
+  },
+  {
+    "revision": "2ff659eda869b8c4512d",
+    "url": "/portfolio.git/static/css/2.dc14db5e.chunk.css"
+  },
+  {
+    "revision": "294a27cb56a600fa12c0",
+    "url": "/portfolio.git/static/css/main.560d43af.chunk.css"
+  },
+  {
+    "revision": "2ff659eda869b8c4512d",
+    "url": "/portfolio.git/static/js/2.5adccefa.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio.git/static/js/2.5adccefa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "294a27cb56a600fa12c0",
+    "url": "/portfolio.git/static/js/main.8efb2090.chunk.js"
+  },
+  {
+    "revision": "2024c32906197a1b3aad",
+    "url": "/portfolio.git/static/js/runtime-main.0b6af4b6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio.git/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "42f7a831df9138ad993edd853e5ca262",
+    "url": "/portfolio.git/static/media/home.42f7a831.jpg"
+  }
+]);
